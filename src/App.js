c93d6ef6b5fb59f631fcbca2202d73a939ad69be
Nasrin200 <nasrin.sama@gmail.com>
2022-03-25 18:25:03 +0430

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App"><h1>Weather App</h1>
+<div className="container">
+      <button className="btn btn-danger">hello</button>
     <footer>This <a href='https://github.com/Nasrin200/weather-in-react' target="_blank">open source</a> app is made by Nasrin Alizadeh</footer>
-    </div>
+    </div></div>
   );
 }
 
