da09e52a855cddbac812c0c9f152b7a88144da7f
Nasrin200 <nasrin.sama@gmail.com>
2022-03-25 17:36:58 +0430

    added GitHub link to my project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App
+    <div className="App"><h1>Weather App</h1>
+    <footer>This <a href='https://github.com/Nasrin200/weather-in-react' target="_blank">open source</a> app is made by Nasrin Alizadeh</footer>
     </div>
   );
 }
