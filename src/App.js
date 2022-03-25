b29660069ab990f2655ca788a0c3b0b9cb272149
Nasrin200 <nasrin.sama@gmail.com>
2022-03-25 20:32:18 +0430

    made Css files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-     <Weather />
+    
 <div className="container">
-  
-    <footer>This <a href='https://github.com/Nasrin200/weather-in-react' target="_blank">open source</a> app is made by Nasrin Alizadeh</footer>
+<Weather />
+    <footer>This <a href='https://github.com/Nasrin200/weather-in-react' target="_blank" className="source">open source</a> app is made by Nasrin Alizadeh</footer>
     </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,43 @@ import React from "react";
 import "./Weather.css"
 export default function Weather () {
     return (
-        <div className="Weather">hello from weather</div>
+      <div className="Weather">
+        <form className="weatherSearch" mt-3>
+        <div className="row">
+            <div className="col-9">
+              <input type="search" 
+              placeholder="Enter a city name" 
+              className="cityName" 
+              autoFocus="on"/>
+            </div>
+            <div className="col-3">
+              <input type="submit" 
+              value="Search" 
+              className="btn btn-primary w-100"/>
+            </div>
+        </div>
+        </form>
+        <h1>Tabriz</h1>
+        <div>
+        <img src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png" className="cloud" />
+        <h2 className="mainDegree">7</h2><span className="unit">°C</span>
+        </div> 
+           <div className="row">
+                <div className="col-6">
+                    <ul>
+                        
+                        <li>Broken cloudes</li>
+                        <li>Friday 18:57</li>
+                    </ul>
+                </div>
+                <div className="col-6">
+                    <ul>
+                      
+                        <li>Humidity: 15%</li>
+                        <li>Wind: 4km/h</li>
+                    </ul>  
+                </div>
+            </div>
+        </div>
     )
 }
