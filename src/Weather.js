1f5f43a171fb8427e8a7553b1a106f39e6d41610
Nasrin200 <nasrin.sama@gmail.com>
2022-03-28 22:18:34 +0430

    did all api jobs

--- src/Weather.js
+++ src/Weather.js
@@ -1,58 +1,77 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
+
 export default function Weather () {
-const [ temperature, setTemoerature] = useState(null);
+   
+const [weatherData, setWeatherData] = useState({ ready:false });
+
 
 function handleResponse (response) {
-console.log(response.data)
+    console.log(response.data)
+    setWeatherData({
+        ready: true,
+        temperature:response.data.main.temp,
+        wind: Math.round(response.data.wind.speed),
+        humidity: response.data.main.humidity,
+        city: response.data.name,
+        iconUrl:"https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png",
+        weatherCondition: response.data.weather[0].description,
+        date:"friday 22:02"
+
+    });
+    
+    }
+    
+if (weatherData.ready) {
+    return (
+    <div className="Weather">
+      <form className="weatherSearch" mt-3>
+      <div className="row">
+          <div className="col-9">
+            <input type="search" 
+            placeholder="Enter a city name" 
+            className="cityName" 
+            autoFocus="on"/>
+          </div>
+          <div className="col-3">
+            <input type="submit" 
+            value="Search" 
+            className="btn btn-primary w-100"/>
+          </div>
+      </div>
+      </form>
+      <h1>{weatherData.city}</h1>
+      <div>
+      <img src={weatherData.iconUrl} className="Cloud" />
+      <h2 className="mainDegree">{Math.round(weatherData.temperature)}</h2><span className="unit">°C</span>
+      </div> 
+         <div className="row">
+              <div className="col-6">
+                  <ul>
+                      
+                      <li>{weatherData.weatherCondition}</li>
+                      <li>{weatherData.date}</li>
+                  </ul>
+              </div>
+              <div className="col-6">
+                  <ul>
+                    
+                      <li>Humidity: {weatherData.humidity}%</li>
+                      <li>Wind: {weatherData.wind}km/h</li>
+                  </ul>  
+              </div>
+          </div>
+      </div>
+  );
+}else{
+    
+           const apiKey="f3c571bd6250dd248124676218b0d8bd";
+     const city="Tabriz"
+           let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        axios.get(apiUrl).then(handleResponse)
+        return "loading..."
 }
 
-   const apiKey="f3c571bd6250dd248124676218b0d8bd";
-   let city="London"
-   let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-axios.get(apiUrl).then(handleResponse)
-
-
-
-    return (
-      <div className="Weather">
-        <form className="weatherSearch" mt-3>
-        <div className="row">
-            <div className="col-9">
-              <input type="search" 
-              placeholder="Enter a city name" 
-              className="cityName" 
-              autoFocus="on"/>
-            </div>
-            <div className="col-3">
-              <input type="submit" 
-              value="Search" 
-              className="btn btn-primary w-100"/>
-            </div>
-        </div>
-        </form>
-        <h1>Tabriz</h1>
-        <div>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png" className="cloud" />
-        <h2 className="mainDegree">7</h2><span className="unit">°C</span>
-        </div> 
-           <div className="row">
-                <div className="col-6">
-                    <ul>
-                        
-                        <li>Broken cloudes</li>
-                        <li>Friday 18:57</li>
-                    </ul>
-                </div>
-                <div className="col-6">
-                    <ul>
-                      
-                        <li>Humidity: 15%</li>
-                        <li>Wind: 4km/h</li>
-                    </ul>  
-                </div>
-            </div>
-        </div>
-    )
+   
 }
